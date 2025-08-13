f18e9bbe2be21e175391473c869e289b7e41608c
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-13 16:30:22 -0500

    Added template for delete method

--- server/app/api/users/route.js
+++ server/app/api/users/route.js
@@ -1,12 +1,16 @@
-//const fs = require('fs');
-//const dataPath = '@/app/util/users.json';
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import users from "@/app/util/users.json"
 import { NextResponse } from "next/server";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+i
+
 export function GET(_, response){
   try {
-    console.log(users)
+    console.log("datapath == ", dataPath)
     return NextResponse.json({users})
   } catch (err) {
     console.error('Error reading users:', err);
@@ -16,5 +20,14 @@ export function GET(_, response){
 }
 
 export function DELETE(_, response){
-    return NextResponse.json({ result : `delete method remove all}`})
+  try {
+    console.log(users)
+    const noUsers = [];
+    console.log("dirname = ", __dirname)
+    //fs.writeFileSync(dataPath, JSON.stringify(noUsers), 'utf8');
+    return NextResponse.json({noUsers, status : 200})
+  } catch (err) {
+    console.error('Error reading users:', err);
+    return [];
+  }
 }

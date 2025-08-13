dbae98c8c50bbc891fa2366c8317fb3f1bb75831
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-13 15:57:43 -0500

    implemented fetching all users 'GET'

--- server/app/api/users/route.js
+++ server/app/api/users/route.js
@@ -1,7 +1,18 @@
+//const fs = require('fs');
+//const dataPath = '@/app/util/users.json';
+
+import users from "@/app/util/users.json"
 import { NextResponse } from "next/server";
 
 export function GET(_, response){
-    return NextResponse.json({ result : `get method fetch all`})
+  try {
+    console.log(users)
+    return NextResponse.json({users})
+  } catch (err) {
+    console.error('Error reading users:', err);
+    return [];
+  }
+  
 }
 
 export function DELETE(_, response){
